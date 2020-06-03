bcc9a3f45470d67975ad36fd7f305613280456f5
mlori <mloria58@gmail.com>
2020-06-03 13:25:26 +0200

    Modified throughput script for all the logs

--- ThroughputEachSteps.py
+++ ThroughputEachSteps.py
@@ -1,6 +1,9 @@
+import operator
+
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from pm4py.objects.conversion.log import converter as converter
 import pandas
+import sys
 
 
 def appendHashMap(hash_map: {}, df: pandas.DataFrame, s: str, concept_name: int, date_index: int):
@@ -24,23 +27,36 @@ def avgThr(values: []):
 
 
 if __name__ == '__main__':
+    indexMap = {"DomesticDeclarationsFiltered.xes": ("(case)_id", 10, 2),
+                "InternationalDeclarationsComplete.xes": ("(case)_id", 23, 2),
+                "PermitLogFiltered.xes": ("case:concept:name", 1, 4),
+                "PrepaidTravelCostFiltered.xes": ("case:concept:name", 19, 2),
+                "RequestForPaymentFiltered.xes": ("case:concept:name", 11, 2)
+                }
     hash_map = {}
-    domesticLog = xes_import_factory.apply("./logs/DomesticDeclarationsFiltered.xes")
-    domesticDF = converter.apply(domesticLog, None, converter.TO_DATA_FRAME)
-    appendHashMap(hash_map, domesticDF, "(case)_id", 10, 2)
-    internationalLog = xes_import_factory.apply("./logs/InternationalDeclarationsFiltered.xes")
-    internationalDF = converter.apply(internationalLog, None, converter.TO_DATA_FRAME)
-    appendHashMap(hash_map, internationalDF, "(case)_id", 23, 2)
-    permitLog = xes_import_factory.apply("./logs/PermitLogFiltered.xes")
-    permitDF = converter.apply(permitLog, None, converter.TO_DATA_FRAME)
-    appendHashMap(hash_map, permitDF, "case:concept:name", 1, 4)
-    prepaidTravelCostLog = xes_import_factory.apply("./logs/PrepaidTravelCostFiltered.xes")
-    prepaidTravelCostDF = converter.apply(prepaidTravelCostLog, None, converter.TO_DATA_FRAME)
-    appendHashMap(hash_map, prepaidTravelCostDF, "case:concept:name", 19, 2)
-    requestForPaymentLog = xes_import_factory.apply("./logs/RequestForPaymentFiltered.xes")
-    requestForPaymentDF = converter.apply(requestForPaymentLog, None, converter.TO_DATA_FRAME)
-    appendHashMap(hash_map, requestForPaymentDF, "case:concept:name", 11, 2)
-    out = open("output.txt", "w")
+    parameter = sys.argv
+    if "-all" in parameter:
+        domesticLog = xes_import_factory.apply("./logs/DomesticDeclarationsFiltered.xes")
+        domesticDF = converter.apply(domesticLog, None, converter.TO_DATA_FRAME)
+        appendHashMap(hash_map, domesticDF, "(case)_id", 10, 2)
+        internationalLog = xes_import_factory.apply("./logs/InternationalDeclarationsComplete.xes")
+        internationalDF = converter.apply(internationalLog, None, converter.TO_DATA_FRAME)
+        appendHashMap(hash_map, internationalDF, "(case)_id", 23, 2)
+        permitLog = xes_import_factory.apply("./logs/PermitLogFiltered.xes")
+        permitDF = converter.apply(permitLog, None, converter.TO_DATA_FRAME)
+        appendHashMap(hash_map, permitDF, "case:concept:name", 1, 4)
+        prepaidTravelCostLog = xes_import_factory.apply("./logs/PrepaidTravelCostFiltered.xes")
+        prepaidTravelCostDF = converter.apply(prepaidTravelCostLog, None, converter.TO_DATA_FRAME)
+        appendHashMap(hash_map, prepaidTravelCostDF, "case:concept:name", 19, 2)
+        requestForPaymentLog = xes_import_factory.apply("./logs/RequestForPaymentFiltered.xes")
+        requestForPaymentDF = converter.apply(requestForPaymentLog, None, converter.TO_DATA_FRAME)
+        appendHashMap(hash_map, requestForPaymentDF, "case:concept:name", 11, 2)
+    else:
+        for i in range(1, len(parameter)):
+            log = xes_import_factory.apply("./logs/" + parameter[i])
+            df = converter.apply(log, None, converter.TO_DATA_FRAME)
+            appendHashMap(hash_map, df, indexMap[parameter[i]][0], indexMap[parameter[i]][1], indexMap[parameter[i]][2])
+    out = open("throughput" + parameter[1] + ".txt", "w")
     for key in hash_map.keys():
         out.write(key + ":" + str(avgThr(hash_map[key])) + "\n")
     out.close()
