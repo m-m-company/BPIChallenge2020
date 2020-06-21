fbb27a2ab7e69bf4a75f8dd8d24019cbd5939520
GitHub <noreply@github.com>
2020-06-21 10:15:52 +0200

    throughput

--- throughputTravelDeclaration.py
+++ throughputTravelDeclaration.py
@@ -1,31 +1,33 @@
+from distutils.log import Log
+
+from pandas._libs.tslibs.timestamps import Timestamp
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from pm4py.objects.conversion.log import converter as converter
-import pandas
+from pandas import DataFrame
 
 
-def avgThr(df: pandas.DataFrame) -> float:
+def avgThr(df: DataFrame) -> float:
     sums = []
     for case_id, group in df.groupby(["(case)_id"]):
-        temp = []
-        for row in group.itertuples():
-            temp.append(row._2)
-        if len(temp) > 1:
-            sum = temp[1] - temp[0]
-            for i in range(1, len(temp) - 1):
-                sum += temp[i + 1] - temp[i]
-            sums.append(sum)
+        time1 = group.iloc[0]["time:timestamp"]
+        tail = group.tail(1)["time:timestamp"].iloc[0]
+        sums.append(tail - time1)
     average = sums[0]
     for i in range(1, len(sums)):
         average = average + sums[i]
+        # print(average)
     return average / len(sums)
 
 
 if __name__ == '__main__':
     domesticLog = xes_import_factory.apply("logs/DomesticDeclarationsFiltered.xes")
-    internationalLog = xes_import_factory.apply("logs/InternationalDeclarationsFiltered.xes")
+    internationalLog = xes_import_factory.apply("logs/InternationalDeclarationsComplete.xes")
+    internationalLogFiltered = xes_import_factory.apply("logs/InternationalDeclarationsFiltered.xes")
     domesticDF = converter.apply(domesticLog, None, converter.TO_DATA_FRAME)
     internationalDF = converter.apply(internationalLog, None, converter.TO_DATA_FRAME)
-    globalDF = domesticDF.append(internationalDF)
+    internationalDFFiltered = converter.apply(internationalLogFiltered, None, converter.TO_DATA_FRAME)
+    # globalDF = domesticDF.append(internationalDF)
     print("domesticDeclaration throughput = ", avgThr(domesticDF))
     print("internationalDeclaration throughput = ", avgThr(internationalDF))
-    print("domesticDeclaration + internationalDeclaration throughput = ", avgThr(globalDF))
+    print("internationalDeclarationFiltered throughput = ", avgThr(internationalDFFiltered))
+    # print("domesticDeclaration + internationalDeclaration throughput = ", avgThr(globalDF))
