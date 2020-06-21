a2052d57e225f3e5b1cab24e5d10a3fd533a69cc
mlori <mloria58@gmail.com>
2020-06-21 11:57:58 +0200

    average throughput per department added

--- findThrouPerDep.py
+++ findThrouPerDep.py
@@ -1,6 +1,6 @@
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from pm4py.objects.conversion.log import converter as converter
-from pandas import DataFrame
+from pandas import DataFrame, Timestamp
 from matplotlib import pyplot as plt, patches as pt, lines as l
 from math import sqrt
 
@@ -14,23 +14,23 @@ def findAvgCostPerDep(df: DataFrame):
         k = group.iloc[0]["(case)_Permit_OrganizationalEntity"]
         if k != "UNKNOWN":
             k = k.split(" ")[2]
-            value = group.iloc[0]["(case)_AdjustedAmount"]
-            average += float(value)
+            value = (group.tail(1)["time:timestamp"].iloc[0] - group.iloc[0]["time:timestamp"]).days
+            average += value
             count += 1
             if k in map.keys():
-                map[k] = (float(map[k][0]) + float(value), map[k][1] + 1)
+                map[k] = (map[k][0] + value, map[k][1] + 1)
             else:
-                map[k] = (float(value), 1)
+                map[k] = (value, 1)
     average = average/count
     for _, group in df.groupby("(case)_id"):
         k = group.iloc[0]["(case)_Permit_OrganizationalEntity"]
         if k != "UNKNOWN":
             k = k.split(" ")[2]
-            value = (float(group.iloc[0]["(case)_AdjustedAmount"]) - average)**2
+            value = ((group.tail(1)["time:timestamp"].iloc[0] - group.iloc[0]["time:timestamp"]).days - average)**2
             if k in variantMap.keys():
-                variantMap[k] = (float(map[k][0]) + float(value), map[k][1] + 1)
+                variantMap[k] = (map[k][0] + value, map[k][1] + 1)
             else:
-                variantMap[k] = (float(value), 1)
+                variantMap[k] = (value, 1)
     for key in variantMap.keys():
         variantMap[key] = sqrt(variantMap[key][0]/variantMap[key][1])
     for key in map.keys():
@@ -47,11 +47,11 @@ def findAvgCostPerDep(df: DataFrame):
     ax.bar(range(len(map)), values, tick_label=names)
     ax.plot(range(len(map)), average, color='red', linestyle='dashed')
     plt.xticks(rotation=90)
-    bluePatch = pt.Patch(color="blue", label="average cost per dep")
+    bluePatch = pt.Patch(color="blue", label="average throughput per dep")
     redLine = l.Line2D([], [], color="red", linestyle="dashed", label="total average")
     plt.legend(handles=[bluePatch, redLine])
     fig.tight_layout()
-    plt.savefig('costPerDepartment.png')
+    plt.savefig('throughputPerDepartment.png')
 
     plt.close(fig)
     fig2, ax2 = plt.subplots()
@@ -60,7 +60,7 @@ def findAvgCostPerDep(df: DataFrame):
     bluePatch = pt.Patch(color="blue", label="mean squared error per dep")
     plt.legend(handles=[bluePatch])
     fig2.tight_layout()
-    plt.savefig('meanSquaredErrorCosts.png')
+    plt.savefig('meanSquaredErrorThroughput.png')
 
 
 if __name__ == '__main__':
