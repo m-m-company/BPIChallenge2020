6d2ac4fbfc2d010e10435c8c5be77dabed3a6190
MattNot <matteoatnotaro@gmail.com>
2020-06-21 10:55:34 +0200

    throughput

--- throughputTravelDeclaration.py
+++ throughputTravelDeclaration.py
@@ -6,16 +6,21 @@ from pm4py.objects.conversion.log import converter as converter
 from pandas import DataFrame
 
 
-def avgThr(df: DataFrame) -> float:
+def avgThr(df: DataFrame, trip: bool = False) -> float:
     sums = []
     for case_id, group in df.groupby(["(case)_id"]):
         time1 = group.iloc[0]["time:timestamp"]
         tail = group.tail(1)["time:timestamp"].iloc[0]
-        sums.append(tail - time1)
+        if trip:
+            startTrip = group.loc[group["concept:name"] == "Start trip"].iloc[0]["time:timestamp"]
+            endTrip = group.loc[group["concept:name"] == "End trip"].iloc[0]["time:timestamp"]
+            tripTime = endTrip - startTrip
+            sums.append(tail - time1 - tripTime)
+        else:
+            sums.append(tail - time1)
     average = sums[0]
     for i in range(1, len(sums)):
         average = average + sums[i]
-        # print(average)
     return average / len(sums)
 
 
@@ -26,8 +31,8 @@ if __name__ == '__main__':
     domesticDF = converter.apply(domesticLog, None, converter.TO_DATA_FRAME)
     internationalDF = converter.apply(internationalLog, None, converter.TO_DATA_FRAME)
     internationalDFFiltered = converter.apply(internationalLogFiltered, None, converter.TO_DATA_FRAME)
-    # globalDF = domesticDF.append(internationalDF)
+    globalDF = domesticDF.append(internationalDFFiltered)
     print("domesticDeclaration throughput = ", avgThr(domesticDF))
-    print("internationalDeclaration throughput = ", avgThr(internationalDF))
+    print("internationalDeclaration throughput = ", avgThr(internationalDF, True))
     print("internationalDeclarationFiltered throughput = ", avgThr(internationalDFFiltered))
-    # print("domesticDeclaration + internationalDeclaration throughput = ", avgThr(globalDF))
+    print("domesticDeclaration + internationalDeclaration throughput = ", avgThr(globalDF))
